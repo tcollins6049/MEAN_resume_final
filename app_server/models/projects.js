a2cd3255525869cb99c017ed5ac9b83de062b498
Tyler Collins <collinstm@appstate.edu>
2024-05-04 04:15:17 -0500

    Initial commit

---
+++ app_server/models/projects.js
@@ -0,0 +1,14 @@
+// models/user.js
+const mongoose = require('mongoose');
+
+const projectsSchema = new mongoose.Schema({
+  proj_name: String,
+  // Organization: String,
+  location: String,
+  startDate: Date,
+  endDate: Date,
+  description: String,
+  images: [String]
+}, { collection: 'projects' });
+
+module.exports = mongoose.model('projects', projectsSchema);
